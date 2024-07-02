efd27eb81b801a979b5242a0a852614147180c0c
Teemu Koivisto <tkoivisto456@gmail.com>
2024-07-02 18:12:35 +0300

    add ssh script

--- ex.sh
+++ ex.sh
@@ -5,6 +5,19 @@ if [ -f .env ]; then
 fi
 
 case "$1" in
+ssh)
+  shift
+  SSH_PATH="$HOME/.ssh/$TF_VAR_ssh_key_name"
+  if [[ -z $1 ]]; then
+    echo -e "Usage: ./ex.sh ssh <ipv4>"
+    exit 1
+  fi
+  if [ ! -f $SSH_PATH ]; then
+    echo -e "\033[41mSSH key not found at: $SSH_PATH\033[0m"
+    exit 1
+  fi
+  ssh -o "IdentitiesOnly=yes" -i $SSH_PATH ubuntu@$1
+  ;;
 tf)
   shift
   terraform $@
